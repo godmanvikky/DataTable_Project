bc22f28c27a9c2436c8e846a4a159469e4246d97
godmanvikky <wish.vignesh95@gmail.com>
2020-01-11 00:09:19 +0530

    Updates

---
+++ precache-manifest.469d576e0dbdc7801f6718b118b8f2f3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b94694c6b61edba94f5632d07a0178d",
+    "url": "/DataTable_Project/index.html"
+  },
+  {
+    "revision": "21bfc43e2d4c961b4b15",
+    "url": "/DataTable_Project/static/css/3.61f0d284.chunk.css"
+  },
+  {
+    "revision": "0c89dbd90848725467eb",
+    "url": "/DataTable_Project/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6f4e9037d5fbd8211c17",
+    "url": "/DataTable_Project/static/js/2.92c450d6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/DataTable_Project/static/js/2.92c450d6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "21bfc43e2d4c961b4b15",
+    "url": "/DataTable_Project/static/js/3.75a8325c.chunk.js"
+  },
+  {
+    "revision": "0c89dbd90848725467eb",
+    "url": "/DataTable_Project/static/js/main.fe61a4bd.chunk.js"
+  },
+  {
+    "revision": "f9319c3e74cf96fdf0d9",
+    "url": "/DataTable_Project/static/js/runtime-main.4440c02f.js"
+  }
+]);
